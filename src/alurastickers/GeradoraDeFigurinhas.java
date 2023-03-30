9396cf81b8784d92f5b3a46f36345396f61607db
Junior Daliessi <juniordaliessi@gmail.com>
2023-03-29 21:20:06 -0300

    Aula 2 parte 3

--- src/alurastickers/GeradoraDeFigurinhas.java
+++ src/alurastickers/GeradoraDeFigurinhas.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.awt.Color;
 import java.awt.Font;
 
@@ -18,13 +19,15 @@ public class GeradoraDeFigurinhas {
     
     public void criar() throws Exception{
         //LEITURA DE IMAGEM
-        InputStream inputStream = new FileInputStream("entrada/Filme.jpg");
+        //InputStream inputStream = new FileInputStream("entrada/Filme.jpg");
+        //BufferedImage imagemOriginal = ImageIO.read(inputStream);
+        InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
         
         //CRIAR UMA NOVA IMAGEM EM MEMÓRIA COM TRANPARENCIA E TAMANHO NOVO
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
-        int novaAltura = altura + 100;
+        int novaAltura = altura + 150;
         BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
         
         //COPIAR A IMAGEM ORIGINAL PRA NOVO IMAGEM (EM MEMÓRIA)
@@ -32,12 +35,12 @@ public class GeradoraDeFigurinhas {
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
         //CONFIGURAR FONTE
-        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 96);
         graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
         
         //ESCREVER UMA FRASE NA NOVA IMAGEM
-        graphics.drawString("TOPZERA", 25, novaAltura - 25);
+        graphics.drawString("TOPZERA", 100, novaAltura - 50);
         
         
         //ESCREVER A NOVA IMAGEM EM UM ARQUIVO
